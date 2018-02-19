50f7568a23ce037219e25e00bba5c43a40e4a787
Chase <chaseit12@gmail.com>
2018-02-19 22:31:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(speakers)
   assignments = []  
   
   speakers.each_with_index { |speaker, index|
-    assignments.push("Hello,#{speaker}! You'll be assigned to room #{(index+1).to_s}!")
+    assignments.push("Hello, #{speaker}! You'll be assigned to room #{(index+1).to_s}!")
   }
   assignments
 end
